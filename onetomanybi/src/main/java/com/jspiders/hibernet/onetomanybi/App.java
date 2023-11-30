a2c16c3224c92482b65c341cc6f5ec6de0494c9a
namratakotkar224 <124867731+namratakotkar224@users.noreply.github.com>
2023-11-30 07:38:01 -0800

    Add files via upload

---
+++ onetomanybi/src/main/java/com/jspiders/hibernet/onetomanybi/App.java
@@ -0,0 +1,13 @@
+package com.jspiders.hibernet.onetomanybi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ onetomanyuni/src/main/java/com/jspiders/hibernet/onetomanyuni/App.java
@@ -0,0 +1,13 @@
+package com.jspiders.hibernet.onetomanyuni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
